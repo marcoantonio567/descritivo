d103523820ed0e343dd04fc5c0b84cb8b98ed6ef
marco antonio feitosa <141979786+marcoantonio567@users.noreply.github.com>
2024-11-17 20:01:08 -0300

    Update textos.py

--- textos.py
+++ textos.py
@@ -1,16 +1,6 @@
 from leitura_excel import *
 
-texto_memorial_descritivo_com_car = f"""
-O imóvel de matrícula {matricula} não possui georreferenciamento averbado em
-matrícula, sua localização foi obtida através de coordenadas Cadastro Ambiental Rural - CAR"""
-
-texto_memorial_descritivo = f"""
-O imóvel de matrícula {matricula} não possui georreferenciamento averbado em
-matrícula, sua localização foi obtida através de coordenadas do memorial
-descritivo e do Cadastro Ambiental Rural - CAR"""
-
-texto_do_gel = """
-•	O imóvel de matrícula n° #matricula  possui georreferenciamento averbado em matrícula, certificado pelo Instituto Nacional de Colonização e Reforma Agrária - INCRA, consoante a certificação nº #gel. Sua localização foi obtida através do mesmo."""
-
-texto_car = f'''O imóvel de matrícula nº {matricula} não possui georeferenciamento averbado em matrícula. 
-Sua localização foi obtida através do Cadastro Ambiental Rural'''
+texto_memorial_descritivo = f'O imóvel de matrícula {matricula} não possui georreferenciamento averbado em matrícula, sua localização foi obtida através de coordenadas do memorial descritivo'
+texto_memorial_descritivo_com_car = f'O imóvel de matrícula {matricula} não possui georreferenciamento averbado em matrícula, sua localização foi obtida através de coordenadas do memorial descritivo e do Cadastro Ambiental Rural - CAR'
+texto_do_gel = f'O imóvel de matrícula n° {matricula}  possui georreferenciamento averbado em matrícula, certificado pelo Instituto Nacional de Colonização e Reforma Agrária - INCRA, consoante a certificação nº {gel}. Sua localização foi obtida através do mesmo.'
+texto_car = f'O imóvel de matrícula nº {matricula} não possui georeferenciamento averbado em matrícula. Sua localização foi obtida através do Cadastro Ambiental Rural'
